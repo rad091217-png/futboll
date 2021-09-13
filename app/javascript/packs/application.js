a40ef2b81c825bec327da60f2769d4fb106736e6
rad091217-png <72019838+rad091217-png@users.noreply.github.com>
2021-09-13 13:40:57 +0900

    fix layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,13 +11,22 @@ require("jquery")
 import "bootstrap"
 
 
-$(function(){
-    setTimeout("$('.time-limit-success').fadeOut('slow')",5000) 
-  })
+$(function () {
+  setTimeout("$('.time-limit-success').fadeOut('slow')", 5000)
+})
 
-  $(function(){
-    setTimeout("$('.time-limit-danger').fadeOut('slow')", 5000) 
-  })
+$(function () {
+  setTimeout("$('.time-limit-danger').fadeOut('slow')", 5000)
+})
+$(function () {
+  //フッターを最下部に固定
+  var $footer = $('#footer');
+  if (window.innerHeight > $footer.offset().top + $footer.outerHeight()) {
+    $footer.attr({
+      'style': 'position:fixed; top:' + (window.innerHeight - $footer.outerHeight()) + 'px;'
+    });
+  }
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
